5516ce562dff0f80a6c2ff8e8de704761462be19
MandyMI35 <mndalwee@gmail.com>
2024-08-04 02:28:39 +0530

    kjk

---
+++ amazon_clone1/lib/features/admin/widgets/category_products_chart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_charts/flutter_charts.dart' as chart;
+
+class CategoryProductsChart extends StatelessWidget {
+  //11.06
+  const CategoryProductsChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return chart.VerticalBarChart(painter:);
+  }
+}
