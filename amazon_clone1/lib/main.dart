048b348837ba791d29adb1ad2b80f4736ba1224a
MandyMI35 <mndalwee@gmail.com>
2024-08-06 00:45:43 +0530

    mnb

--- amazon_clone1/lib/main.dart
+++ amazon_clone1/lib/main.dart
@@ -2,13 +2,11 @@ import 'package:amazon_clone1/common/widgets/bottom_bar.dart';
 import 'package:amazon_clone1/constants/global_variables.dart';
 import 'package:amazon_clone1/features/admin/screens/admin_screen.dart';
 import 'package:amazon_clone1/features/auth/screens/auth_screen.dart';
-//import 'package:amazon_clone1/features/home/screens/home_screen.dart';
 import 'package:amazon_clone1/providers/user_provider.dart';
 import 'package:amazon_clone1/router.dart';
 import 'package:amazon_clone1/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-//import 'package:amazon_clone1/router.dart';
 
 void main() {
   runApp(MultiProvider(providers: [
